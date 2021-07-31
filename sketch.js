398099ab70bed20a2c1134a7dff9bc9efc958658
aarushg605 <87419811+aarushg605@users.noreply.github.com>
2021-07-31 09:44:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ seaImg=loadImage("sea.png")
+ shipImg1=loadAnnimation
+ spriteName.addImage(seaImg)
+ if(sea.x<0){
+   sea.x=sea.Width/2
+ }
+}
